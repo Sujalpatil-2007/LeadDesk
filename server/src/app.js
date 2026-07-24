94847b7238d98a74c8f116121354b1d2d7e57408
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-24 21:12:24 +0530

    base api

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,19 @@
 const cookieParser = require("cookie-parser");
-const express = require("express"); 
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("../src/routes/auth.routes")
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
+app.use("/api/auth",authRoutes);
 
 module.exports = app;
