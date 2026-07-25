fa6b13a077b78e73fad7ca5e731243a4c8b947c5
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-25 17:54:27 +0530

    dashboard and login page updates

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,60 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import toast from "react-hot-toast";
+
+import DashboardCards from "../components/DashboardCards";
+import SearchBar from "../components/SearchBar";
+import Topbar from "../components/Topbar";
+import LeadTable from "../components/LeadTable";
+import StatusFilter from "../components/StatusFilter";
+
 function Dashboard() {
+  const [leads, setLeads] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("All");
+
+  const getLeads = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await api.get(`/lead?search=${search}&status=${status}`);
+
+      setLeads(data.leads);
+    } catch (error) {
+      toast.error("Failed to fetch leads");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLeads();
+  }, [search, status]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <h2 className="text-xl font-semibold">Loading...</h2>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
+    <div className="min-h-screen bg-slate-100">
+      <div className="max-w-7xl mx-auto p-6">
+        <Topbar />
+
+        <DashboardCards leads={leads} />
+
+        <div className="flex flex-col md:flex-row gap-4 my-8">
+          <SearchBar search={search} setSearch={setSearch} />
+
+          <StatusFilter status={status} setStatus={setStatus} />
+        </div>
+
+        <LeadTable leads={leads} getLeads={getLeads} />
+      </div>
     </div>
   );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,99 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../api/axios";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return toast.error("Please fill all fields");
+    }
+
+    try {
+      setLoading(true);
+
+      const { data } = await api.post("/auth/login", formData);
+
+      toast.success(data.message);
+
+      navigate("/admin");
+
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "Login failed"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin Login</h1>
+    <>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+
+      <div className="bg-white w-full max-w-md rounded-2xl shadow-xl p-8">
+
+        <h1 className="text-3xl font-bold text-center mb-2">
+          Admin Login
+        </h1>
+
+        <p className="text-center text-gray-500 mb-8">
+          Sign in to manage your leads
+        </p>
+
+        <form onSubmit={handleSubmit} className="space-y-5">
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full border rounded-lg px-4 py-3 outline-none focus:border-blue-600"
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full border rounded-lg px-4 py-3 outline-none focus:border-blue-600"
+          />
+
+          <button
+            disabled={loading}
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white py-3 rounded-lg"
+          >
+            {loading ? "Signing In..." : "Login"}
+          </button>
+
+          <span className="text-sm ">Go back to <a className="text-blue-600 hover:underline" href="/">Home</a></span>
+
+        </form>
+
+      </div>
+
     </div>
+    </>
   );
 }
 
